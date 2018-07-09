612acf8161c78f0672cd4e8f7c6b4b020c1951f1
Deepti <deeptiarora01>
2018-07-09 08:22:11 +0530

    Added validation to select atleast one file.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -3,25 +3,19 @@ package controllers;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import model.FileDto;
-import model.Size;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 import service.ImageProcessor;
-import service.ImageProcessorImpl;
 import service.UploadToS3;
-import service.UploadToS3Impl;
-
 import views.html.*;
 /**
  * This class is the home controller used for index and upload page.
@@ -50,30 +44,36 @@ public class HomeController extends Controller {
     public Result upload() throws IOException {
         final Http.MultipartFormData<File> formData = request().body().asMultipartFormData();
         final List<Http.MultipartFormData.FilePart<File>> fileParts = formData.getFiles();
-        
-        // For each file selected from frontend.
-        for(Http.MultipartFormData.FilePart<File> filePart:fileParts){
-	        	
-        	   // upload original file to S3.
-	        	File file = filePart.getFile();
-	        	FileDto fileDto = fileToFileDtoTransformer(file,filePart.getFilename(),filePart.getContentType(),"original");
-	        	String path = file.getPath();
-				uploadToS3.upload(fileDto);
-				
-				// resize smaller by 50% and then upload to S3
-	          double percent = 0.5;
-				File fileSmall = imageProcessor.resize(path, path+"small", percent);
-				FileDto fileDtoSmall = fileToFileDtoTransformer(fileSmall,filePart.getFilename(),filePart.getContentType(),"small");
-				uploadToS3.upload(fileDtoSmall);
-				
-				// resize bigger by 50% and the upload to S3
-	          percent = 1.5;
-	          File fileBig= imageProcessor.resize(path, path+"bigger", percent);
-				FileDto fileDtoBig = fileToFileDtoTransformer(fileBig,filePart.getFilename(),filePart.getContentType(),"big");
-				uploadToS3.upload(fileDtoBig);
-     
+        // Execute the code only if there is atleast one file selected by user
+        if (!"".equals(fileParts.get(0).getFilename())) {
+	        // For each file selected from frontend.
+	        for(Http.MultipartFormData.FilePart<File> filePart:fileParts){
+		        	
+	        	   // upload original file to S3.
+		        	File file = filePart.getFile();
+		        	FileDto fileDto = fileToFileDtoTransformer(file,filePart.getFilename(),filePart.getContentType(),"original");
+		        	String path = file.getPath();
+					uploadToS3.upload(fileDto);
+					
+					// resize smaller by 50% and then upload to S3
+		          double percent = 0.5;
+					File fileSmall = imageProcessor.resize(path, path+"small", percent);
+					FileDto fileDtoSmall = fileToFileDtoTransformer(fileSmall,filePart.getFilename(),filePart.getContentType(),"small");
+					uploadToS3.upload(fileDtoSmall);
+					
+					// resize bigger by 50% and the upload to S3
+		          percent = 1.5;
+		          File fileBig= imageProcessor.resize(path, path+"bigger", percent);
+					FileDto fileDtoBig = fileToFileDtoTransformer(fileBig,filePart.getFilename(),filePart.getContentType(),"big");
+					uploadToS3.upload(fileDtoBig);
+	     
+	        }
+	        return ok("Files Uploaded to S3");
+        }else{
+        	// here comes the validation
+         //flash("error", "Missing file");
+            return ok("There are no file to be uploaded");    
         }
-        return ok("Files Uploaded to S3");
     }
 
     private FileDto fileToFileDtoTransformer(File file,String name,String contentType,String fileType) throws IOException{
